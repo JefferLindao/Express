8cb98b5d642b0b5df0e31bae006f3fc02ac72a90
JefferLindao <jefferlindao@gmail.com>
2021-04-10 19:37:00 -0500

    Manejo de archivos estaticos

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -3,8 +3,10 @@ const path = require('path')
 const productsRouter = require('./routes/products');
 const app = express();
 
-app.set("views", path.join(__dirname, "views"));
+app.use("/static", express.static(path.join(__dirname, "public")));
+
 app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
 app.use('/products', productsRouter);
 
