d109d82e25be5ae7cfe83b1a6c5356348ae85b4d
JefferLindao <jefferlindao@gmail.com>
2021-04-10 20:42:58 -0500

    Anatomia de una APIRESTfull e implementando CRUD

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path')
 const productsRouter = require('./routes/products');
+const productsApiRouter = require('./routes/api/products');
 const app = express();
 
 app.use("/static", express.static(path.join(__dirname, "public")));
@@ -9,6 +10,8 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 app.use('/products', productsRouter);
+
+app.use("/api/products", productsApiRouter);
 
 
 const server = app.listen(8000, function () {
