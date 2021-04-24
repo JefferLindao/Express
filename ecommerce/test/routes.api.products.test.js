9d6dc226c673a69f79eca018f9f5edb5b70e483c
JefferLindao <jefferlindao@gmail.com>
2021-04-23 19:10:00 -0500

    Creando tests para nuestros endpoins 2

--- ecommerce/test/routes.api.products.test.js
+++ ecommerce/test/routes.api.products.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const proxyquire = require('proxyquire');
 
-const { productsMock, ProductsServiceMock } = require('../util/mocks/products');
+const { mocksProducts, ProductsServiceMock } = require('../util/mocks/products');
 
 const testServer = require('../util/testServer');
 
@@ -13,6 +13,27 @@ describe("routes - api - products", function () {
   describe("GET /products", function () {
     it("should respond with status 200", function (done) {
       request.get('/api/products').expect(200, done);
+    })
+
+    it("should respond with content type json", function (done) {
+      request.get('/api/products').expect("Content-type", /json/, done);
+    })
+
+    it("should respond with not error", function (done) {
+      request.get('/api/products').end((err, res) => {
+        assert.strictEqual(err, null);
+        done();
+      })
+    })
+
+    it("should respond with list of products", function (done) {
+      request.get('/api/products').end((err, res) => {
+        assert.deepEqual(res.body, {
+          data: mocksProducts,
+          message: "products listed"
+        })
+        done();
+      })
     })
   })
 })
